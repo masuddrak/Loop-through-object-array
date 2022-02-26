bf491aa95736f84bdabdf5400561d34087a27952
masudrana <masudranaacb@gmail.com>
2022-02-26 17:06:05 +0600

    using a object & array loop through

---
+++ app.js
@@ -0,0 +1,17 @@
+// for(let i=0; i<10; i++){}
+// for(const friend of friends){} array
+// for(const friend in friends){} object
+// using a object  
+const bottols={
+    name:'tiktok-bottol',
+    color:'red',
+    price:222,
+}
+for(const bottol in bottols){
+    // console.log(bottol)
+}
+// using array
+const friends=['sakib','rakib','sanu']
+for(const friend of friends){
+    // console.log(friend)
+}
